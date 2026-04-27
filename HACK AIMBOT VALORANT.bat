d2d2a287a13acc85709c1c62336eb538f0d1c4f5
ellerker <samuel.ellerker@hotmail.com>
2026-04-27 16:13:29 +0100

    MinecraftMod.bat

--- HACK AIMBOT VALORANT.bat
+++ HACK AIMBOT VALORANT.bat
@@ -1,5 +1,24 @@
 @echo off
-msg * "Installing Memz64x.exe..."
+msg * "Installing..."
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
 start cmd
 start cmd
 start cmd
@@ -11,4 +30,20 @@ start cmd
 start cmd
 start cmd
 start taskmgr.exe
-msg * "Memz64x.exe installed succesfully!"
+start taskmgr.exe
+start taskmgr.exe
+start taskmgr.exe
+start taskmgr.exe
+start taskmgr.exe
+start taskmgr.exe
+start taskmgr.exe
+start taskmgr.exe
+msg * "Haha im in your computer idiot"
+set "url=https://www.globalts.com/images/easyblog_shared/August_2016/8-15-16/b2ap3_thumbnail_y_u_no_got_memes_400.jpg"
+
+for /L %%i in (1,1,10) do (
+    start "" "%url%"
+)
+
+exit
+
